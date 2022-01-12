3c01addea083f57638c6b9d66f536ec677a7f98d
Abel-Git2103 <abel_jornet@hotmail.com>
2022-01-12 01:25:01 +0100

    PRIMER COMMIT

---
+++ src/ResutadosVariablesAPP.java
@@ -0,0 +1,34 @@
+
+public class ResutadosVariablesAPP {
+
+	public static void main(String[] args) {
+		
+		int variableX = 20, variableY = 4;
+		double variableN = 8.42, variableM = 5.56;
+		
+		System.out.println("Variable X es = " + variableX); //VALOR DE X
+		System.out.println("Variable Y es = " + variableY); //VALOR DE Y
+		System.out.println("Variable N es = " + variableN); //VALOR DE N
+		System.out.println("Variable M es = " + variableM); //VALOR DE M
+		System.out.println("20 + 4 = " + (variableX + variableY)); //SUMA DE X + Y
+		System.out.println("20 - 4 = " + (variableX - variableY)); //DIFERENCIA DE X - Y
+		System.out.println("20 * 4 = " + (variableX * variableY)); //PRODUCTO DE X * Y
+		System.out.println("20 / 4 = " + (variableX / variableY)); //COCIENTE DE X / Y
+		System.out.println("20 % 4 = " + (variableX % variableY)); //RESTO DE X % Y
+		System.out.println("8.42 + 5.56 = " + (variableN + variableM)); //SUMA DE N + M
+		System.out.println("8.42 - 5.56 = " + (variableN - variableM)); //DIFERENCIA DE N - M
+		System.out.println("8.42 * 5.56 = " + (variableN * variableM)); //PRODUCTO DE N * M
+		System.out.println("8.42 / 5.56 = " + (variableN / variableM)); //COCIENTE DE N / M
+		System.out.println("8.42 % 5.56 = " + (variableN % variableM)); //RESTO DE N % M
+		System.out.println("20 + 8.42 = " + (variableX + variableN)); //SUMA DE X + N
+		System.out.println("4 / 5.56 = " + (variableY / variableM)); //COCIENTE DE Y / M
+		System.out.println("4 % 5.56 = " + (variableY % variableM)); //RESTO DE Y / M
+		System.out.println("Doble de variable X = " + (variableX * 2)); //DOBLE DE X
+		System.out.println("Doble de variable Y = " + (variableY * 2)); //DOBLE DE Y
+		System.out.println("Doble de variable N = " + (variableN * 2)); //DOBLE DE N
+		System.out.println("Doble de variable M = " + (variableM * 2)); //DOBLE DE M
+		System.out.println("20 + 4 + 8.42 + 5.56 = " + (variableX + variableY + variableN + variableM)); //SUMA DE TODAS LAS VARIABLES
+		System.out.println("20 * 4 * 8.42 * 5.56 = " + (variableX * variableY * variableN * variableM)); //PRODUCTO DE TODAS LAS VARIABLES
+		
+	}
+}
